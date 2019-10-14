ad921d6a07b62d7735038e49e9c536a822e01812
Julen Pérez Hernández <i72pehej@uco.es>
2019-10-14 17:57:44 +0200

    p2 alg uni

--- Algoritmica/Practica2/p2.cpp
+++ Algoritmica/Practica2/p2.cpp
@@ -17,51 +17,93 @@
 
 
 
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-
 ////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char const *argv[]) {
   system("clear");  // Se limpia la terminal
 
   int opcion ;  // Variable para el switch
+
   int columnaFinal; // Valor de la columna a la que se quiere llegar
+  std::vector< std::vector<int> > matriz;
+  int nCaminos = 0; // Numero de caminos para cada posicion
 
 
-  std::cout << "Introduzca la columna de la fila 8 a la que desea llegar: ";
-  std::cin >> columnaFinal;
+
+
+  std::cout << "\n\t--------------------------------------------------------" << '\n';
+  std::cout << "\t(1) ------ CAMINOS PARA LLEGAR A LA FILA 8 DESDE LA 1" << '\n';
+  std::cout << "\t(2) ------ CAMINOS PARA LLEGAR A LA FILA 8 DESDE (1 , 2)" << '\n';
+  std::cout << "\t(3) ------ CAMINOS PARA LLEGAR A LA FILA 8 DESDE (1 , 7)" << '\n';
+  std::cout << "\t--------------------------------------------------------" << '\n';
+  std::cout << "\t(4) ------ VISUALIZAR SOLUCIONES" << '\n';
+  std::cout << "\t(5) ------ CAMINOS PARA LLEGAR A LA FILA n DESDE LA 1" << '\n';
+  std::cout << "\t--------------------------------------------------------" << '\n';
+  std::cout << "\t(0) ------ SALIR" << '\n';
+  std::cout << "\t--------------------------------------------------------" << '\n';
+  std::cout << "\nIntroduzca el apartado que desea realizar: ";
+  std::cin >> opcion;
   std::cout << '\n';
 
-  while ((columnaFinal < 1) || (columnaFinal > 8)) {
-    std::cout << "\n\nERROR." << "\nPor favor, introduzca un valor entre 1 y 8: ";
-    std::cin >> columnaFinal;
-    std::cout << '\n';
-  }
-
-
-  // PULSE INTRO PARA CONTINUAR
-  std::cout << "\n\n" << '\n';
-
-
+////////////////////////////////////////////////////////////////////////////////
 
   switch (opcion) {
     case 0:
+      system("clear");  // Se limpia la terminal
+      std::cout << "Terminando ejecucion . . .\n\n" << '\n';
 
+      return 0;
     break;
 
     case 1:
+      system("clear");  // Se limpia la terminal
+
+      std::cout << "Introduzca la columna de la fila 8 a la que desea llegar (0 a 7): ";
+      std::cin >> columnaFinal;
+      std::cout << '\n';
+
+      // Comprobacion para que los valores esten en el tablero
+      while ((columnaFinal < 0) || (columnaFinal > 7)) {
+        std::cout << "\n\nERROR." << "\nPor favor, introduzca un valor entre 0 y 7: ";
+        std::cin >> columnaFinal;
+        std::cout << '\n';
+      }
+
+      matriz = std::vector< std::vector<int> > (8, std::vector<int> (8)); // Reserva de memoria
+      calcularCaminos(7, columnaFinal, nCaminos, matriz); // Calcular los caminos
+
+      std::cout << "Caminos = " << nCaminos << '\n';
+    break;
+
+////////////////////////////////////////////////////////////////////////////////
+
+    case 2:
+      system("clear");  // Se limpia la terminal
 
     break;
+
+////////////////////////////////////////////////////////////////////////////////
+
+    case 3:
+      system("clear");  // Se limpia la terminal
+
+    break;
+
+////////////////////////////////////////////////////////////////////////////////
+
+    case 4:
+      system("clear");  // Se limpia la terminal
+
+    break;
+
+////////////////////////////////////////////////////////////////////////////////
+
+    case 5:
+      system("clear");  // Se limpia la terminal
+
+    break;
+
+////////////////////////////////////////////////////////////////////////////////
   }
 
 
--- Algoritmica/Practica2/p2.hpp
+++ Algoritmica/Practica2/p2.hpp
@@ -2,6 +2,75 @@
 #define P2_HPP
 
 
+////////////////////////////////////////////////////////////////////////////////
+
+// Imprime el camino que se reconoce de la matriz
+void imprimeMatriz(const std::vector< std::vector<int> > &m) {
+  std::cout << "Camino: ";
+  for (int i = 0; i < m.size(); i++) {
+    for (int j = 0; j < m.size(); j++) {
+      // Posicion del movimiento de la ficha
+      if (m[i][j] == 1) {
+        std::cout << "|X|";
+      }
+      else {
+        std::cout << "| |";
+      }
+    }
+    std::cout << '\n';
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Funcion para calcular los caminos posibles comenzando desde el final
+void calcularCaminos(const int fila, const int columna, int &nCaminos, std::vector< std::vector<int> > &m) {
+  // Si se llega al principio del tablero, se ha encontrado un camino
+  if (fila == 0) {
+    m[fila][columna] = 1;   // Asignamos posicion valida
+    // imprimeMatriz(m);
+    m[fila][columna] = -1;  // Reincio del valor para continuar la busqueda del camino
+
+    nCaminos++;
+  }
+  // Se realiza la comprobacion de caminos con las 4 posibles posiciones de las
+  else {
+    m[fila][columna] = 1; // Colocamos
+
+    // Posicion 1
+    if (columna > 1) {  // Evita que salga del tablero
+      calcularCaminos(fila - 1, columna - 2, nCaminos, m);
+    }
+
+    // Posicion 2
+    if ((fila > 1) && (columna > 0)) {  // Evita que salga del tablero
+      calcularCaminos(fila - 2, columna - 1, nCaminos, m);
+    }
+
+    // Posicion 3
+    if ((fila > 1) && (columna < 7)) {  // Evita que salga del tablero
+      calcularCaminos(fila - 2, columna + 1, nCaminos, m);
+    }
+
+    // Posicion 4
+    if (columna < 6) {  // Evita que salga del tablero
+      calcularCaminos(fila - 1, columna + 2, nCaminos, m);
+    }
+  }
+
+  m[fila][columna] = -1;
+}
 
 
 
