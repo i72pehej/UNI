aaaff5cd98d88807cc97c6cfcef0f8ea33b449ac
Julen Pérez Hernández <43370446+i72pehej@users.noreply.github.com>
2019-10-29 19:19:24 +0100

    UNI-CASA-MV

--- Algoritmica/Practica3/p3.cpp
+++ Algoritmica/Practica3/p3.cpp
@@ -12,8 +12,33 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char const *argv[]) {
-  std::cout << "/* message */" << '\n';
+  int opcion; // Opcion para switch
 
+
+  std::cout << "\n\n-------------------------------" << '\n';
+  std::cout << "Introduzca la operacion deseada" << '\n';
+  std::cout << "-------------------------------" << '\n';
+  std::cout << "(1) ------ SUMA" << '\n';
+  std::cout << "(2) ------ PRODUCTO" << '\n';
+  std::cout << "-------------------------------" << '\n';
+  std::cout << "(0) ------ SALIR" << '\n';
+  std::cout << "-------------------------------" << '\n';
+
+
+
+  switch (opcion) {
+    case 0: // SALIR
+      return 0;
+    break;
+
+    case 1: // SUMA
+
+    break;
+
+    case 2: // PRODUCTO
+
+    break;
+  }
 
 
   return 0;
--- Algoritmica/Practica3/p3.hpp
+++ Algoritmica/Practica3/p3.hpp
@@ -1,6 +1,13 @@
 #ifndef P3_HPP
 #define P3_HPP
 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <math.h>
+#include <iostream>
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -10,18 +17,51 @@ class Entero {
     std::string entero_; // Numero entero en formato de string
     int tamMax_; // Tamaño maximo del entero
 
+    // Funciones privadas
+    bool es_numero(const std::string &s);
+    int setTam(const int t);
+
   public:
-    Entero (const Entero num, const int tam);  // Constructor
+    Entero ();  // Constructor
     virtual ~Entero (); // Destructor
 
     // Observadores
-    std::string getEnteroStr();
+    std::string getEnteroString();
     int getEnteroInt();
     int getTamMax();
 
     // Modificadores
-    void setEntero();
+    void setEntero(const std::string str);
 };
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// METODOS PRIVADOS ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Funcion que comprueba si un string contiene valores numericos
+bool Entero::es_numero(const std::string &s) {
+  std::string::const_iterator it = s.begin();
+
+  while (it != s.end() && std::isdigit(*it)) {++it;}
+
+  // Devuelve false cuando el string esta vacio y no se ha llegado al final
+  // Devuelve true cuando el string no esta vacio y se ha llegado al final = son numeros
+  return (!s.empty() && it == s.end());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Funcion que asigna un valor entero a la variable tamMax_
+void Entero::setTam(const int t) {
+  getTamMax() = t;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////// METODOS PUBLICOS ////////////////////////////////
@@ -31,39 +71,45 @@ class Entero {
 ////////////////////////////////////////////////////////////////////////////////
 
 // Constructor de la clase Entero
-Entero (const Entero num, const int tam) {
-  this -> entero_ = "";
-  this -> tamMax_ = 4;
+Entero::Entero () {
+  setEntero("");
+  setTam(4);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Devuelve el numero de la clase Entero en formato "string"
-void getEnteroStr() {
-
+// Funcion que devuelve el valor de la clase Entero en formato int
+std::string Entero::getEnteroString() {
+  return this->entero_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Devuelve el numero de la clase Entero en formato "int"
-void getEnteroInt() {
-
+// Funcion que devuelve el valor de la clase Entero en formato int
+int Entero::getEnteroInt() {
+  // int res = std::stoi(getEnteroString());
+  // return res;
+  return (std::stoi(getEnteroString(), NULL, 0));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Devuelve el tamaño maximo de digitos del valor de Entero
-void getTamMax() {
-
+// Funcion que devuelve el tamaño maximo del Entero
+int Entero::getTamMax() {
+  return this->tamMax_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Asigna un valor a la clase Entero
-void setEntero() {
-
+// Funcion que asigna un valor a la clase Entero
+void Entero::setEntero(std::string str) {
+  getEnteroString() = str;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 
@@ -84,20 +130,33 @@ Entero &operator +(Entero &res, const Entero e1, const Entero e2) {
 ////////////////////////////////////////////////////////////////////////////////
 
 // Sobrecarga del operador del producto
-Entero &operator *(Entero &res, const Entero e1, const Entero e2) {
+Entero &operator *(const Entero e2) {
 
-
-  return res;
+  return e2;
+  // return pow(w*y, 2s) + pow10(w*y + y*z, s);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // Sobrecarga del operador de entrada (std::cin >>;)
 istream &operator >>(Entero &entrada, istream &in) {
-  cout << "Introduzca el valor (debe ser menor de 4 digitos): ";
-
+  cout << "Introduzca el numero: ";
   in >> entrada.getEntero();
   in.ignore();
+
+  // while (in.size() != getTamMax()) {  // Comprueba que se introduzcan 4 valores
+  //   std::cout << "\n\nPor favor, introduzca un numero de maximo 4 digitos" << '\n';
+  //   std::cout << "VALOR: ";
+  //   in >> entrada.getEntero();
+  //   in.ignore();
+
+    while (!es_numero(in)) {  // Comprueba que los 4 valores sean numericos
+      std::cout << "\n\nPor favor, introduzca un numero" << '\n';
+      std::cout << "VALOR: ";
+      in >> entrada.getEntero();
+      in.ignore();
+    }
+  }
 
   return in;
 }
