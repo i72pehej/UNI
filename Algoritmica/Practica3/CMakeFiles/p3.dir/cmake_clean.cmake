2ba777c0f029050c5a087f69c5672cd258c2b849
Julen <i72pehej@uco.es>
2019-11-01 20:19:15 +0100

    CASA-Juaguli

---
+++ Algoritmica/Practica3/CMakeFiles/p3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p3.dir/p3.cpp.o"
+  "p3.pdb"
+  "p3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
