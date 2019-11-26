ec617140810225cae89f8d50811a0d0eb59e6526
Julen <i72pehej@uco.es>
2019-11-26 22:52:51 +0100

    REVERSE

---
+++ Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/algorithm.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/algorithm.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/digitalcurve.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/digitalcurve.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/dynamicprogrammingmethod.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/dynamicprogrammingmethod.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/line.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/line.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/main.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/main.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/point.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/point.cpp.o"
+  "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/suppressioncollinearpointsmethod.cpp" "/home/julen/i72pehej/Uni_3/Algoritmica/Practica5/Codigo/CMakeFiles/main.x.dir/suppressioncollinearpointsmethod.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
