271979f8b7489fff486c4ec8cc0ed61e9f00e903
Julen <i72pehej@uco.es>
2019-11-27 21:43:06 +0100

    ALG P5 FIN 2.0

--- Algoritmica/Practica5/Codigo/dynamicprogrammingmethod.cpp
+++ Algoritmica/Practica5/Codigo/dynamicprogrammingmethod.cpp
@@ -1,5 +1,7 @@
 #include <vector>    // necesario para el contenedor vector
 #include <limits>
+#include <algorithm>	// Para utilizar "reverse()"
+
 #include "algorithm.hpp"
 #include "digitalcurve.hpp"
 #include "dynamicprogrammingmethod.hpp"
@@ -27,33 +29,19 @@ DynamicProgrammingMethod::~DynamicProgrammingMethod() {}
 
 ////////////////////////////////////////////////////////////////////////////////
 
-int minimoError(int i, int j) {
+void DynamicProgrammingMethod::rellenarMatrizErrores(int const puntosCurvaOriginal, std::vector< std::vector<long double> > &matrizErrores) {
+	matrizErrores[1][1] = 0;
 
+	for (int i = 2; i <= puntosCurvaOriginal; i++) {
+		matrizErrores[i][1] = std::numeric_limits<long double>::max();	// Infinito
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void DynamicProgrammingMethod::apply() {
-	int puntosCurvaOriginal = getOriginalCurve().getNumberPointsDigitalCurve();	// Numero de puntos de la curva original
-	int puntosParaAproximar = getNumberPointsPolygonalApproximation();	// Numero de puntos con los que se quiere aproximar
-
+void DynamicProgrammingMethod::rellenarMatrizFathers(int const puntosParaAproximar, int const puntosCurvaOriginal, std::vector< std::vector<long double> > &matrizErrores, std::vector< std::vector<long double> > &matrizFathers) {
 	long double error1, error2;	// Errores entre las aproximaciones seleccionadas
-	long double puntoPredecesor;	// Valor para el predecesor de la aproximacion deseada
 
-	std::vector<int> vecDominantes;	// Vector de puntos dominantes
-
-	std::vector< std::vector<long double> > matrizErrores(puntosCurvaOriginal + 1, std::vector<long double>(puntosParaAproximar + 1));	// Matriz de errores acumulados
-	std::vector< std::vector<long double> > matrizFathers(puntosCurvaOriginal + 1, std::vector<long double>(puntosParaAproximar + 1));	// Matriz de puntos predecesores
-
-
-	// Se calculan los sumatorios para los errores
-	calculateSummations();
-
-	// Se asignan los valores de los casos particulares de errores acumulados
-	matrizErrores[1][1] = 0;
-	for (int i = 2; i <= puntosCurvaOriginal; i++) {
-		matrizErrores[i][1] = std::numeric_limits<long double>::max();	// Infinito
-	}
 
 	// Se itera por los valores de "m"
 	for (int m = 2; m <= puntosParaAproximar; m++) {
@@ -75,20 +63,53 @@ void DynamicProgrammingMethod::apply() {
 			}
 		}
 	}
+}
 
-	// Se insertan en orden los valores de la matriz de puntos predecesores en el vector de puntos dominantes
-	vecDominantes.insert(vecDominantes.begin(), puntosCurvaOriginal - 1);	// Se inserta el ultimo punto dominante, correspondiente al ultimo punto de la curva
+////////////////////////////////////////////////////////////////////////////////
+
+std::vector<int> DynamicProgrammingMethod::fathersToDominantes(int const puntosParaAproximar, int const puntosCurvaOriginal, std::vector< std::vector<long double> > const matrizFathers) {
+	long double puntoPredecesor;	// Valor para el predecesor de la aproximacion deseada
+	std::vector<int> vecDominantes;	// Vector de puntos dominantes
+
 
 	puntoPredecesor = matrizFathers[puntosCurvaOriginal][puntosParaAproximar];	// Se asigna el valor de la aproximacion inicial deseada
-	vecDominantes.insert(vecDominantes.begin(), puntoPredecesor - 1);	// Se inserta el predecesor al punto de la aproximacion
+	// Se insertan en orden los valores de la matriz de puntos predecesores en el vector de puntos dominantes
+	vecDominantes.push_back(puntosCurvaOriginal - 1);	// Se inserta el ultimo punto dominante, correspondiente al ultimo punto de la curva
+	vecDominantes.push_back(puntoPredecesor - 1);	// Se inserta el predecesor al punto de la aproximacion
 
+	// Se rellena el vector de puntos dominantes de la curva
 	for (int j = puntosParaAproximar - 1; puntoPredecesor > 1; j--) {
-		vecDominantes.insert(vecDominantes.begin(), matrizFathers[puntoPredecesor][j] - 1);
-		puntoPredecesor = matrizFathers[puntoPredecesor][j];
+		vecDominantes.push_back(matrizFathers[puntoPredecesor][j] - 1);	// Se hace "-1" para que el valor introducido este correctamente indexado
+		puntoPredecesor = matrizFathers[puntoPredecesor][j];	// Se asigna el nuevo punto predecesor
 	}
 
+	// Se invierte el vector para ordenar los puntos dominantes
+	std::reverse(vecDominantes.begin(), vecDominantes.end());
+
+	return vecDominantes;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void DynamicProgrammingMethod::apply() {
+	int puntosCurvaOriginal = getOriginalCurve().getNumberPointsDigitalCurve();	// Numero de puntos de la curva original
+	int puntosParaAproximar = getNumberPointsPolygonalApproximation();	// Numero de puntos con los que se quiere aproximar
+
+	std::vector< std::vector<long double> > matrizErrores(puntosCurvaOriginal + 1, std::vector<long double>(puntosParaAproximar + 1));	// Matriz de errores acumulados
+	std::vector< std::vector<long double> > matrizFathers(puntosCurvaOriginal + 1, std::vector<long double>(puntosParaAproximar + 1));	// Matriz de puntos predecesores
+
+
+	// Se calculan los sumatorios para los errores
+	calculateSummations();
+
+	// Se asignan los valores de los casos particulares de errores acumulados a la matriz de errores
+	rellenarMatrizErrores(puntosCurvaOriginal, matrizErrores);
+
+	// Se asignan los predecesores para cada punto de la matriz "fathers"
+	rellenarMatrizFathers(puntosParaAproximar, puntosCurvaOriginal, matrizErrores, matrizFathers);
+
 	// Se asigna el vector de puntos dominantes final al objeto actual
-	setDominantPointsPosition(vecDominantes);
+	setDominantPointsPosition(fathersToDominantes(puntosParaAproximar, puntosCurvaOriginal, matrizFathers));
 
 ////////////////////////////////////////////////////////////////////////////////
 
--- Algoritmica/Practica5/Codigo/dynamicprogrammingmethod.hpp
+++ Algoritmica/Practica5/Codigo/dynamicprogrammingmethod.hpp
@@ -6,6 +6,11 @@
 //This class implements a method for obtaining a polygonal approximation
 class DynamicProgrammingMethod:public Algorithm {
 	private:
+		void rellenarMatrizErrores(int const puntosCurvaOriginal, std::vector< std::vector<long double> > &matrizErrores);
+
+		void rellenarMatrizFathers(int const puntosParaAproximar, int const puntosCurvaOriginal, std::vector< std::vector<long double> > &matrizErrores, std::vector< std::vector<long double> > &matrizFathers);
+
+		std::vector<int> fathersToDominantes(int const puntosParaAproximar, int const puntosCurvaOriginal, std::vector< std::vector<long double> > const matrizFathers);
 
 	public:
 		DynamicProgrammingMethod(char * fileName, int numberPointsPolygonalApproximation);
